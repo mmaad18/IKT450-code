b758359b3cfd0080830248a377341508037b8bdc
mmaad18 <61145345+mmaad18@users.noreply.github.com>
2024-10-18 00:08:10 +0200

    Assignment 4 | Added dataset and mode

---
+++ assignments/convolutional_neural_networks_4/Food11Dataset.py
@@ -0,0 +1,31 @@
+import os
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+from torchvision.io import read_image
+
+from assignments.DatasetMode import DatasetMode as DM
+
+
+class Food11Dataset(Dataset):
+    def __init__(self, file_path, mode=DM.TRAIN, split_ratio=0.8):
+        self.file_path = file_path
+        self.mode = mode
+        self.split_ratio = split_ratio
+
+        self.img_labels = [
+            'Bread', 'Dairy product', 'Dessert', 'Egg', 'Fried food', 'Meat', 'Noodles-Pasta', 'Rice', 'Seafood', 'Soup', 'Vegetable-Fruit'
+        ]
+
+
+    def __len__(self):
+        return len(self.img_labels)
+
+
+    def __getitem__(self, index):
+        img_path = os.path.join(self.file_path, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+
+        return torch.tensor(X, dtype=torch.float32), torch.tensor(Y, dtype=torch.float32)
+
+
