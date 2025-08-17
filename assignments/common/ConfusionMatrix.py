9e60029dfa1bbbb7335f8253f52be26a2ee3d333
mmaad18 <61145345+mmaad18@users.noreply.github.com>
2025-08-17 17:38:19 +0200

    Added most of metrics to ConfusionMatrix

--- assignments/common/ConfusionMatrix.py
+++ assignments/common/ConfusionMatrix.py
@@ -19,6 +19,19 @@ class ConfusionMatrix:
         self.matrix[true_index, predicted_index] += 1
 
 
+    def update_vector(self, Y_val: NDArray[np.int32], Y_pred: NDArray[np.int32], reset: bool=False):
+        if len(Y_val) != len(Y_pred):
+            raise ValueError("Length of true labels and predicted labels must be the same.")
+
+        if reset:
+            self.matrix = np.zeros((self.size, self.size), dtype=np.int32)
+
+        K = self.matrix.shape[0]
+        idx = Y_val * K + Y_pred
+        cm = np.bincount(idx, minlength=K * K).reshape(K, K)
+        self.matrix += cm
+
+
     def full_update(self, matrix: NDArray[np.int32]):
         self.matrix = matrix
 
@@ -27,10 +40,44 @@ class ConfusionMatrix:
         return np.sum(self.matrix, axis=1)
 
 
-    def precision(self, label_index: int) -> float:
-        true_positive = self.matrix[label_index,label_index]
-        false_positive = np.sum(self.matrix[:, label_index]) - true_positive
-        return true_positive / (true_positive + false_positive) if (true_positive + false_positive) > 0 else 0.0
+    def grand_total(self) -> int:
+        return np.sum(self.matrix)
+
+
+    def precision(self) -> NDArray[np.float64]:
+        return np.diag(self.matrix) / np.sum(self.matrix, axis=0)
+
+
+    def recall(self) -> NDArray[np.float64]:
+        return np.diag(self.matrix) / np.sum(self.matrix, axis=1)
+
+
+    def macro_average_precision(self) -> float:
+        return np.mean(self.precision())
+
+
+    def macro_average_recall(self) -> float:
+        return np.mean(self.recall())
+
+
+    def macro_f1_score(self) -> float:
+        precision = self.macro_average_precision()
+        recall = self.macro_average_recall()
+        return 2 * (precision * recall) / (precision + recall)
+
+
+    def micro_average_precision(self) -> float:
+        return np.sum(self.precision()) / self.grand_total()
+
+
+    def micro_average_recall(self) -> float:
+        return np.sum(self.recall()) / self.grand_total()
+
+
+    def micro_f1_score(self) -> float:
+        precision = self.micro_average_precision()
+        recall = self.micro_average_recall()
+        return 2 * (precision * recall) / (precision + recall)
 
 
     def accuracy(self) -> float:
@@ -51,6 +98,32 @@ class ConfusionMatrix:
         nominator = np.sum((self.matrix.diagonal() / actual_total) * weights)
         denominator = self.matrix.shape[0] * np.sum(weights)
         return nominator / denominator if denominator > 0 else 0.0
+
+
+    def MCC(self):
+        c = np.sum(self.matrix.diagonal())
+        s = np.sum(self.matrix)
+
+        p = np.sum(self.matrix, axis=0)
+        t = np.sum(self.matrix, axis=1)
+
+        sum1 = np.sum(p * t)
+        sum2 = np.sum(p**2)
+        sum3 = np.sum(t**2)
+
+        return (c * s - sum1) / np.sqrt((s**2 - sum2) * (s**2 - sum3))
+
+
+    def cohens_kappa(self):
+        c = np.sum(self.matrix.diagonal())
+        s = np.sum(self.matrix)
+
+        p = np.sum(self.matrix, axis=0)
+        t = np.sum(self.matrix, axis=1)
+
+        sum1 = np.sum(p * t)
+
+        return (c * s - sum1) / (s**2 - sum1)
 
 
     def plot(self, title: str = "Confusion Matrix"):
--- assignments/common/ConfusionMatrixTest.py
+++ assignments/common/ConfusionMatrixTest.py
@@ -23,6 +23,97 @@ class MyTestCase(unittest.TestCase):
         np.testing.assert_array_equal(asserted, expected)
 
 
+    def test_precision(self):
+        asserted = self.confusion_matrix.precision()
+        expected = np.array([6 / 11, 9 / 11, 10 / 13, 12 / 17])
+
+        np.testing.assert_array_equal(asserted, expected)
+
+
+    def test_recall(self):
+        asserted = self.confusion_matrix.recall()
+        expected = np.array([6 / 9, 9 / 14, 10 / 13, 12 / 16])
+
+        np.testing.assert_array_equal(asserted, expected)
+
+
+    def test_macro_average_precision(self):
+        asserted = self.confusion_matrix.macro_average_precision()
+
+        precision = np.array([6 / 11, 9 / 11, 10 / 13, 12 / 17])
+        expected = np.mean(precision)
+
+        self.assertEqual(asserted, expected)
+
+
+    def test_macro_average_recall(self):
+        asserted = self.confusion_matrix.macro_average_recall()
+        recall = np.array([6 / 9, 9 / 14, 10 / 13, 12 / 16])
+        expected = np.mean(recall)
+
+        self.assertEqual(asserted, expected)
+
+
+    def test_macro_f1_score(self):
+        asserted = self.confusion_matrix.macro_f1_score()
+        macro_average_precision = np.mean(np.array([6 / 11, 9 / 11, 10 / 13, 12 / 17]))
+        macro_average_recall = np.mean(np.array([6 / 9, 9 / 14, 10 / 13, 12 / 16]))
+        expected = 2 * (macro_average_precision * macro_average_recall) / (macro_average_precision + macro_average_recall)
+
+        self.assertEqual(asserted, expected)
+
+
+    def test_micro_average_precision(self):
+        asserted = self.confusion_matrix.micro_average_precision()
+        precision = np.array([6 / 11, 9 / 11, 10 / 13, 12 / 17])
+        expected = np.sum(precision) / 52
+
+        self.assertEqual(asserted, expected)
+
+
+    def test_micro_average_recall(self):
+        asserted = self.confusion_matrix.micro_average_recall()
+        recall = np.array([6 / 9, 9 / 14, 10 / 13, 12 / 16])
+        expected = np.sum(recall) / 52
+
+        self.assertEqual(asserted, expected)
+
+
+    def test_micro_f1_score(self):
+        asserted = self.confusion_matrix.micro_f1_score()
+        micro_average_precision = np.sum(np.array([6 / 11, 9 / 11, 10 / 13, 12 / 17])) / 52
+        micro_average_recall = np.sum(np.array([6 / 9, 9 / 14, 10 / 13, 12 / 16])) / 52
+        expected = 2 * (micro_average_precision * micro_average_recall) / (micro_average_precision + micro_average_recall)
+
+        self.assertEqual(asserted, expected)
+
+
+    def test_MCC(self):
+        asserted = self.confusion_matrix.MCC()
+
+        c = 6 + 9 + 10 + 12
+        s = 52
+
+        sum1 = 9 * 11 + 14 * 11 + 13 * 13 + 16 * 17
+        sum2 = 11**2 + 11**2 + 13**2 + 17**2
+        sum3 = 9**2 + 14**2 + 13**2 + 16**2
+
+        expected = (c * s - sum1) / np.sqrt((s**2 - sum2) * (s**2 - sum3))
+
+        self.assertEqual(asserted, expected)
+
+
+    def test_cohens_kappa(self):
+        asserted = self.confusion_matrix.cohens_kappa()
+
+        c = 6 + 9 + 10 + 12
+        s = 52
+
+        sum1 = 9 * 11 + 14 * 11 + 13 * 13 + 16 * 17
+
+        expected = (c * s - sum1) / (s**2 - sum1)
+
+
     def test_accuracy(self):
         asserted = self.confusion_matrix.accuracy()
         expected = (6 + 9 + 10 + 12) / 52
